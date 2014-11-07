4cfb62f014cb7705bec3220b97cc02fc8c2909f5
Ruben <lordders@gmail.com>
2014-11-07 16:47:24 +0100

    7

---
+++ src/General.java
@@ -0,0 +1,46 @@
+public class Habitante
+{
+	// Propiedades / Atributos
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private String dni;
+	private String nacionalidad;
+	private date fecha_nacimiento;
+	private String sexo;
+
+	// Metodo / Constructor
+	public Circulos (double valorRadio)
+	{
+		this.radio = valorRadio;
+	}
+}
+
+
+public class Inmueble
+{
+	private String localidad;
+	private String direccion;
+	private String tipo;
+	private date fecha_compra;
+	private date fecha_venta;
+}
+
+public class EspacioPublico
+{
+
+}
+
+public class Ayuntamiento
+{
+	private int numero;
+	private String localidad;
+	private String direccion;
+}
+
+public class Partido
+{
+	private String nombre;
+	private String presidente;
+	private int numero;
+}
